c188db7bf4cf6b0dee351baf1fb76258760d5797
last-arg <last_arg@tutanota.com>
2021-11-25 21:20:10 +0200

    Add flex-basis rule. Return object (instead of string) in rule functions (where applicable)

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,6 +6,7 @@ import fs from "fs";
 const config = defineConfig({
   rules: [
     [/^stack\-?(\d*)(\w*)$/, ruleStack],
+    [/^basis\-(\d+)(\w*)$/, ruleFlexBasis],
     [/^l-grid-?(.*)$/, ruleLayoutGrid, {layer: "component"}],
   ],
   shortcutsLayer: "component",
@@ -13,7 +14,7 @@ const config = defineConfig({
     ["btn", "px-3 py-2 rounded-sm"],
     ["btn-pocket", "text-sky-500 border-2 border-sky-400 hover:bg-sky-400 hover:text-white"],
     ["textbox", "max-w-50ch w-30ch min-h-8em border-2 rounded-sm border-truegray-300"],
-    ["fieldset-wrapper", "bg-indigo-50 p-4"],
+    ["fieldset-wrapper", "bg-indigo-50 p-4 flex-1 basis-10"],
     ["input-wrapper", "bg-indigo-100 p-2 w-max"],
     ["legend-title", "text-lg border-b-4 border-indigo-100 text-indigo-900"],
   ],
@@ -30,11 +31,10 @@ const config = defineConfig({
 });
 
 function ruleStack([selector, nr, unit]) {
-  console.log("stack")
-  const classSelector = "." + escapeSelector(selector)
   const css_attr = "--space"
 
   if (nr === '' && unit === '') {
+    const classSelector = "." + escapeSelector(selector)
     return `
 ${classSelector} { display: flex; flex-direction: column; justify-content: flex-start; }
 ${classSelector} > template + *,
@@ -47,15 +47,22 @@ ${classSelector} > * + * { margin-top: var(${css_attr}, 1.5rem); }
     `
   }
 
-  if (unit !== '') return `${classSelector} { ${css_attr}: ${nr}${unit}; }`
-  if (nr !== '') return `${classSelector} { ${css_attr}: ${nr / 4}rem; }`
+  if (unit !== '') return { [css_attr]: `${nr}${unit}` }
+  if (nr !== '') return { [css_attr]: `${nr / 4}rem` }
 
-  return `/* Failed to generate stack rule from ${selector} */`
+  return `/* Error: Failed to generate stack rule from ${selector} */`
+}
+
+function ruleFlexBasis([selector, nr, unit]) {
+  if (unit !== '') return { "flex-basis": `${nr}${unit}` }
+  if (nr !== '') return { "flex-basis": `${nr / 4}rem` }
+
+  return `/* Error: Failed to generate flex-basis rule from ${selector} */`
 }
 
 async function ruleLayoutGrid([selector, min_width], {generator}) {
-  const classSelector = "." + escapeSelector(selector)
   if (min_width === '') {
+    const classSelector = "." + escapeSelector(selector)
     return `
 ${classSelector} {
   display: grid;
@@ -70,9 +77,9 @@ ${classSelector} {
 
   const [,,attrs] = await generator.parseUtil(min_width)
   const value = attrs[0][1]
-  if (value) return `${classSelector} { --grid-min: ${value} }`
+  if (value) return { "--grid-min": value }
 
-  return `/* Failed to generate l-grid rule from ${selector} */`
+  return `/* Error: Failed to generate l-grid rule from ${selector} */`
 }
 
 export default config
